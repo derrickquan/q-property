d451f02588c3011acf86cdd7027aca3508f1ca0a
derrickquan <dkquan@gmail.com>
2025-08-10 11:50:41 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+// pages/_app.tsx
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import NavBar from "../components/NavBar";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const showNav = router.pathname !== "/"; // avoid double header on the landing page
+
+  return (
+    <>
+      {showNav && <NavBar />}
+      <Component {...pageProps} />
+    </>
+  );
 }
